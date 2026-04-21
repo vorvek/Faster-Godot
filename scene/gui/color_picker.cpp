5672af16a182f994d3af5f7c67ed0ed4127b5cdf
SorkoPiko <me@sorko.dev>
2026-04-21 15:12:55 +1000

    fix: only show color picker permission box when sampler is visible
    (cherry picked from commit 82680c25d4c3c50d13d21cddd0ddf3d23e141557)

--- scene/gui/color_picker.cpp
+++ scene/gui/color_picker.cpp
@@ -91,7 +91,7 @@ void ColorPicker::_notification(int p_what) {
 #ifdef MACOS_ENABLED
 		case NOTIFICATION_VISIBILITY_CHANGED: {
 			if (is_visible_in_tree()) {
-				perm_hb->set_visible(!OS::get_singleton()->get_granted_permissions().has("macos.permission.RECORD_SCREEN"));
+				perm_hb->set_visible(sampler_visible && !OS::get_singleton()->get_granted_permissions().has("macos.permission.RECORD_SCREEN"));
 			}
 		} break;
 #endif
@@ -1928,6 +1928,9 @@ void ColorPicker::set_sampler_visible(bool p_visible) {
 	}
 	sampler_visible = p_visible;
 	sample_hbc->set_visible(p_visible);
+#ifdef MACOS_ENABLED
+	perm_hb->set_visible(p_visible && !OS::get_singleton()->get_granted_permissions().has("macos.permission.RECORD_SCREEN"));
+#endif
 }
 
 bool ColorPicker::is_sampler_visible() const {
@@ -2303,7 +2306,6 @@ ColorPicker::ColorPicker() {
 	perm_link->connect(SceneStringName(pressed), callable_mp(this, &ColorPicker::_req_permission));
 	perm_hb->add_child(perm_link);
 	real_vbox->add_child(perm_hb);
-	perm_hb->set_visible(false);
 }
 
 void ColorPicker::_req_permission() {
