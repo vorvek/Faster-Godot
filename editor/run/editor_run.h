b30493f572c70acb94998c8ca58c26fafd47d2da
Thaddeus Crews <repiteo@outlook.com>
2026-04-27 13:46:11 -0500

    Revert "[HDR] Save editor screenshot as EXR when HDR is enabled."
    This reverts commit d97199bcbf71bf9151d6296a600f23f9ab3aadf9.
    
    (cherry picked from commit 6e2b9cbab40430051e840b18edf7f03154e52df5)

--- editor/run/editor_run.h
+++ editor/run/editor_run.h
@@ -33,7 +33,7 @@
 #include "core/os/os.h"
 
 typedef void (*EditorRunInstanceStarting)(int p_index, List<String> &r_arguments);
-typedef bool (*EditorRunInstanceRequestScreenshot)(const Callable &p_callback, bool p_hdr);
+typedef bool (*EditorRunInstanceRequestScreenshot)(const Callable &p_callback);
 
 class EditorRun {
 public:
@@ -73,7 +73,7 @@ public:
 	int get_child_process_count() const { return pids.size(); }
 	OS::ProcessID get_current_process() const;
 
-	static bool request_screenshot(const Callable &p_callback, bool p_hdr);
+	static bool request_screenshot(const Callable &p_callback);
 
 	static WindowPlacement get_window_placement();
 
