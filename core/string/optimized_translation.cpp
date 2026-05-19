05b7aa681d9e0ab64d1f07bcbacba187f58d723a
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2026-05-19 10:55:37 +0300

    [Import] Fix empty columns importing as invalid English translation.
    (cherry picked from commit 735827bd78ac05d9768cb3ebfc65746cd50698fd)

--- core/string/optimized_translation.cpp
+++ core/string/optimized_translation.cpp
@@ -29,6 +29,7 @@
 /**************************************************************************/
 
 #include "optimized_translation.h"
+#include "optimized_translation.compat.inc"
 
 #include "core/templates/pair.h"
 
@@ -42,11 +43,11 @@ struct CompressedString {
 	int offset = 0;
 };
 
-void OptimizedTranslation::generate(const Ref<Translation> &p_from) {
+bool OptimizedTranslation::generate(const Ref<Translation> &p_from) {
 	// This method compresses a Translation instance.
 	// Right now, it doesn't handle context or plurals.
 #ifdef TOOLS_ENABLED
-	ERR_FAIL_COND(p_from.is_null());
+	ERR_FAIL_COND_V(p_from.is_null(), false);
 
 	List<StringName> keys;
 	{
@@ -149,7 +150,7 @@ void OptimizedTranslation::generate(const Ref<Translation> &p_from) {
 		bucket_table_size += 2 + b.size() * 4;
 	}
 
-	ERR_FAIL_COND(bucket_table_size == 0);
+	ERR_FAIL_COND_V(bucket_table_size == 0, false);
 
 	hash_table.resize(size);
 	bucket_table.resize(bucket_table_size);
@@ -188,9 +189,12 @@ void OptimizedTranslation::generate(const Ref<Translation> &p_from) {
 		memcpy(&cw[compressed[i].offset], compressed[i].compressed.get_data(), compressed[i].compressed.size());
 	}
 
-	ERR_FAIL_COND(btindex != bucket_table_size);
+	ERR_FAIL_COND_V(btindex != bucket_table_size, false);
 	set_locale(p_from->get_locale());
 
+	return true;
+#else
+	return false;
 #endif
 }
 
